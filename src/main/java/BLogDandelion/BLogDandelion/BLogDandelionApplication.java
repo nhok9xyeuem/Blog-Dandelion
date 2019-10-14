08a6d7807a49a71e0079bb79d66047270582d7f7
nhok9xyeuem <nhuconghaui@gmail.com>
2019-10-14 10:13:20 +0700

    add spring boot

---
+++ src/main/java/BLogDandelion/BLogDandelion/BLogDandelionApplication.java
@@ -0,0 +1,13 @@
+package BLogDandelion.BLogDandelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BLogDandelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BLogDandelionApplication.class, args);
+	}
+
+}
