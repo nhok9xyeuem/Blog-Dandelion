fb1d9b828751a41c02de69db8cb3cebd30519713
nhok9xyeuem <nhuconghaui@gmail.com>
2019-10-15 13:58:15 +0700

    xoa bo theme

--- src/main/java/BLogDandelion/BLogDandelion/controller/ThemeController.java
+++ src/main/java/BLogDandelion/BLogDandelion/controller/ThemeController.java
@@ -7,7 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 public class ThemeController {
@@ -21,4 +27,20 @@ public class ThemeController {
         modelAndView.addObject("themes", theme);
         return modelAndView;
     }
+    @GetMapping("/delete-theme/{id}")
+    public ModelAndView themeDeleteForm(@PathVariable Long id ){
+        Optional<Theme> theme = themeService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("theme/delete");
+        modelAndView.addObject("themes",theme.get());
+        return modelAndView;
+    }
+    //waive - từ bỏ ::::
+    @PostMapping("/waive/{id}")
+    public  ModelAndView themeDelete(@RequestParam Long id ,RedirectAttributes redirectAttributes){
+        themeService.remove(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/theme");
+        redirectAttributes.addFlashAttribute("message", "DELETE THEME FINISH");
+        return modelAndView;
+    }
+
 }
