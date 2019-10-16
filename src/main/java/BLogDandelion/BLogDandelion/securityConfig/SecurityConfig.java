ff6e7d6b7b3ce70bd385edcf84424d6fd5303c5f
nhok9xyeuem <nhuconghaui@gmail.com>
2019-10-16 11:09:30 +0700

    blog

--- src/main/java/BLogDandelion/BLogDandelion/securityConfig/SecurityConfig.java
+++ src/main/java/BLogDandelion/BLogDandelion/securityConfig/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(
                         "/title",
-                        "/login"
+                        "/login",
+                        "/theme"
                         //                so tang tat ca moi ng vao duoc
                 ).permitAll()
 
